aee007c68b97c6ca45696a3a74e194e732d79275
rockleeviu <56508456+rockleeviu@users.noreply.github.com>
2023-04-22 22:42:25 +0300

    changed db from local to cloud

--- app.js
+++ app.js
@@ -16,8 +16,8 @@ const User = require('./models/user')
 const mongoSanitize = require('express-mongo-sanitize');
 const helmet = require('helmet');
 
-// const dbUrl = process.env.DB_URL;
-const dbUrl = 'mongodb://localhost:27017/yelp-camp';
+const dbUrl = process.env.DB_URL;
+// const dbUrl = 'mongodb://localhost:27017/yelp-camp';
 
 const userRoutes = require('./routes/users');
 const campgroundRoutes = require('./routes/campgrounds');

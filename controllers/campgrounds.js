e0899090e7aa30f7b6eebefb7e5f83746dd963d5
rockleeviu <56508456+rockleeviu@users.noreply.github.com>
2023-04-22 22:13:45 +0300

    commit with Ghighe

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -1,7 +1,7 @@
 const Campground = require('../models/campground');
 const { cloudinary } = require("../cloudinary");
 const mbxGeocoding = require("@mapbox/mapbox-sdk/services/geocoding");
-const mapBoxToken = process.env.MAPBOX_Token;
+const mapBoxToken = process.env.MAPBOX_TOKEN;
 const geocoder = mbxGeocoding({ accessToken: mapBoxToken });
 
 
